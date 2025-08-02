6afa0c1bf3b6d599c04c32a28f3b3aa9b2a0d5e3
scoobyff <pranishwarnetha02@gmail.com>
2025-08-03 01:42:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,8 +7,17 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   swcMinify: true,
-  experimental: {
-    appDir: true,
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
   },
 }
 
